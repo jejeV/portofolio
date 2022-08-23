60c213e9e9872dbb7a84a9a0de7362d94c5354ec
jejeV <muejason27@gmail.com>
2022-08-23 15:07:25 +0700

    first commit

---
+++ src/components/Particles.vue
@@ -0,0 +1,70 @@
+<template>
+  <Particles
+    id="tsparticles"
+    :options="options"
+    :particlesInit="particlesInit"
+  />
+</template>
+
+<script lang="ts">
+import { Engine } from "tsparticles-engine";
+// import { loadFull } from 'tsparticles';
+import { loadSlim } from "tsparticles-slim";
+
+export default {
+  data() {
+    return {
+      options: {
+        background: {
+          // color: "#000"
+        },
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+          },
+          nodes: {
+            push: {
+              quantity: 10,
+            },
+            respulse: {
+              distance: 150,
+            },
+          },
+        },
+        particles: {
+          links: {
+            distance: 70,
+            enable: true,
+          },
+          move: {
+            enable: true,
+            speed: { min: 1, max: 5 },
+          },
+          opacity: {
+            value: { min: 0.2, max: 0.2 },
+          },
+          size: {
+            value: { min: 0.5, max: 2 },
+          },
+        },
+      },
+    };
+  },
+  methods: {
+    async particlesInit(engine: Engine) {
+      await loadSlim(engine);
+    },
+  },
+};
+</script>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,57 @@
+<script setup>
+import WelcomeItem from "./WelcomeItem.vue";
+import FIGMAIcon from "./icons/IconFIGMA.vue";
+import PHPIcon from "./icons/IconPHP.vue";
+import JSIcon from "./icons/IconJS.vue";
+import HTMLIcon from "./icons/IconHTML.vue";
+import CSSIcon from "./icons/IconCSS.vue";
+</script>
+
+<template>
+  <h1 style="font-weight: 600; color: skyblue">Skill</h1>
+  <hr />
+  <WelcomeItem>
+    <template #icon>
+      <FIGMAIcon />
+    </template>
+    <template #heading>FIGMA</template>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur
+    consequuntur aperiam placeat ducimus expedita suscipit?
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <PHPIcon />
+    </template>
+    <template #heading>PHP</template>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid, aperiam!
+    Accusamus praesentium a possimus libero?
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <JSIcon />
+    </template>
+    <template #heading>JAVASCRIPT</template>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sequi quidem
+    voluptatibus voluptates alias fugiat?
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <HTMLIcon />
+    </template>
+    <template #heading>HTML</template>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita assumenda
+    nulla sequi adipisci tempora officia?
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CSSIcon />
+    </template>
+    <template #heading>CSS</template>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe officiis
+    autem sunt. Exercitationem, consequatur natus.
+  </WelcomeItem>
+</template>
